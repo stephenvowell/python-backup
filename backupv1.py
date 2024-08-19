c2b928b47ac3810880449a593e364e03e57aa6cb
Stephen Vowell <vowellstephen@gmail.com>
2024-08-19 17:06:56 -0500

    changed t

--- backupv1.py
+++ backupv1.py
@@ -123,7 +123,7 @@ def on_start():
 
 # Create the main window
 root = tk.Tk()
-root.title("Backup Scheduler for Joe")
+root.title("Backup Scheduler for Python")
 
 # Source folder
 tk.Label(root, text="Source Folder:").grid(row=0, column=0, padx=10, pady=5)
